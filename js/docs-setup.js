e8e91863e350fa2124b6d77acff8bb83b28beb93
Michal Dvorak (cen38289) <michadvorak@csas.cz>
2015-02-01 17:25:12 +0100

    Updates

---
+++ js/docs-setup.js
@@ -0,0 +1,25 @@
+NG_DOCS={
+  "sections": {
+    "api": "Angular DataRouter Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "apiHref",
+      "shortName": "apiHref",
+      "type": "directive",
+      "moduleName": "apiHref",
+      "shortDescription": "Translates API URL into view URL and sets it as href. It is replacement for ngHref directive.",
+      "keywords": "$datarouterprovider $scope _blank _self ac angular api api-href apihref apiprefix application attribute base behave behavior browser button case changes code config configured controller data detail directive download downloaded error example external force full function hasbang href html html5mode ignored image inside js link links mapped maps mdvorakdatarouter media mode module navigates navigation ng-controller nghref opens optional parent performed performs points prefetched redirect reload replacement resource sample samplectrl scenarios screen server set sets specification supported supports target template templateurl translates triggers type url users view window"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
